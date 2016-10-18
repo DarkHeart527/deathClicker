6b8be3d80eb457ba4f72314d3d4b156bd091fa44
DarkHeart527 <darkheart527s@gmail.com>
2016-10-18 13:58:25 -0700

    Update js.js

--- js.js
+++ js.js
@@ -41,30 +41,9 @@ Page 14 - First Time
 
 /* Set Variables */
 
-var taxAndFee = 1.25;
-
 /* Required Variables */
-var timePlayed = null;
-var Money = null;
-var clickPower = null;
-var perSecond = null;
 
-var firstTime;
-
-function declare() {
- Money = Money;
- clickPower = clickPower;
- timePlayed = timePlayed;
- perSecond = perSecond;
- document.getElementById('timeArea').innerHTML = timePlayed;
- document.getElementById('moneyArea').innerHTML = Money;
-}
- 
 //\/_____________________________________________________________PAGE 3______________________________________________________________________________/\//
-
-function addKill() {
-	Money = Money + clickPower;
-}
 
 //\/_____________________________________________________________PAGE 4______________________________________________________________________________/\//
 
@@ -74,62 +53,17 @@ function addKill() {
 
 //\/_____________________________________________________________PAGE 7______________________________________________________________________________/\//
 
-function addTime() {
-	timePlayed = timePlayed + 1;
-}
-
 //\/_____________________________________________________________PAGE 8______________________________________________________________________________/\//
-function saver() {
-	localStorage.Moneystat = Money;
-	localStorage.clickPowerstat = clickPower;
-	localStorage.timePlayedstat = timePlayed;
-	localStorage.perSecondstat = perSecond;
-}
 
 //\/_____________________________________________________________PAGE 9______________________________________________________________________________/\//
 
-function statLoader() {
-	Money =  Number(localStorage.Moneystat);
-	clickPower =  Number(localStorage.clickPowerstat);
-	timePlayed = Number(localStorage.timePlayedstat);
-	firstTime = localStorage.getItem('firstTimestat');
-}
 //\/_____________________________________________________________PAGE 10______________________________________________________________________________/\//
-
-function timeDisplay() {
-	console.info ("| Time Played: ",timePlayed,"| Click Power: ", clickPower,"| Money: ", Money,"| Money Per Second: ", perSecond);
-}
 
 //\/_____________________________________________________________PAGE 11______________________________________________________________________________/\//
 
-function perSecondMoney() {
-	Money = Money + perSecond;
-}
-
 //\/_____________________________________________________________PAGE 12______________________________________________________________________________/\//
 
-function loadIntervals() {
-	setInterval( function() { addTime(); }, 1000);
-	setInterval( function() { timeDisplay(); }, 1000);
-	setInterval( function() { declare(); }, 1);
-	setInterval( function() { perSecondMoney(); }, 1000);
-	setInterval( function() { saver() }, 10);
-	setTimeout( function() { statLoader() }, 5);
-	setTimeout( function() { checkJoinTime() }, 6);
-}
-
 //\/_____________________________________________________________PAGE 13______________________________________________________________________________/\//
-
-function reset() {
-	timePlayed = 0;
-	Money = 0;
-	clickPower = 1;
-	perSecond = 0;
-	localStorage.Moneystat = 0;
-	localStorage.clickPowerstat = 1;
-	localStorage.timePlayedstat = 0;
-	localStorage.perSecond = 0;
-}
 
 //\/_____________________________________________________________PAGE 14______________________________________________________________________________/\//
 
