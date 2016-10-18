09cf76706560b3c58011d6862a0d5eda96ae0cd5
DarkHeart527 <darkheart527s@gmail.com>
2016-10-18 14:35:31 -0700

    Update js.js

--- js.js
+++ js.js
@@ -76,11 +76,11 @@ function checkJoinTime() {
 	 alert('Welcome');
 	 reset();
 	 localStorage.join = 1;
-	} else if (localStorage.join = 0) {
+	} else if (localStorage.join = "0") {
 	 alert('Welcome');
 	 reset();
 	 localStorage.join = 1;
-	} else if (localStorage.join = 1) {
+	} else if (localStorage.join = "1") {
 	 alert('Welcome Back');
 	}
 }
